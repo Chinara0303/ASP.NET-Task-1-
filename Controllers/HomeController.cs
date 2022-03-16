e4fe2816bbd3dd7a00706cb29af795beb29d3c85
[Chinara0303] <chinaraei@code.edu.az>
2022-03-16 12:02:25 +0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int? id,string name)
+        {
+            if (id != null)
+            {
+                if (id > 0)
+                {
+                    return Content($"sifirdan boyukdu {name}");
+                }
+                return Content($"sifirdan boyuk deyil {name}");
+
+            }
+            return View();
+
+        }
+        public IActionResult About(int? id, string name)
+        {
+            if (id != null)
+            {
+                if (id > 0)
+                {
+                    return Content($"sifirdan boyukdu {name}");
+                }
+                return Content($"sifirdan boyuk deyil {name}");
+
+            }
+            return View();
+        }
+        public IActionResult Contact(int? id, string name)
+        {
+            if (id != null)
+            {
+                if (id > 0)
+                {
+                    return Content($"sifirdan boyukdu {name}");
+                }
+                return Content($"sifirdan boyuk deyil {name}");
+
+            }
+            return View();
+        }
+    }
+}
